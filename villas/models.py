cbb86db637c11d9d4ab3ebb6ab071fff9da37bad
soroush <soroush8506@gmail.com>
2026-07-14 02:07:53 +0330

    added models for Villa and Availability

--- villas/models.py
+++ villas/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Villa(models.Model):
+
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='villas')
+    title = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    address = models.TextField()
+    price_per_night = models.IntegerField()
+    capacity = models.PositiveIntegerField()
+    amenities = models.JSONField(default=list, blank=True)
+
+
+class Availability(models.Model):
+
+    villa = models.ForeignKey(Villa, on_delete=models.CASCADE, related_name='availabilities')
+    date = models.DateField()
+    is_available = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('villa', 'date')

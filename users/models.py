960ee19c1d2535f7d1f099b6e0542f40d3949db3
Ehsan <e.motevasel@gmail.com>
2026-07-14 01:02:13 +0330

    Added users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    ROLES = [('host', 'Host'), ('guest', 'Guest')]
+
+    full_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLES)
+    created_at = models.DateTimeField(auto_now_add=True)

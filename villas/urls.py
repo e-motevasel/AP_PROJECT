388a512c674bb44c0c86138405f6a531409189aa
soroush <soroush8506@gmail.com>
2026-07-15 15:01:01 +0330

    completed the availabilty api

--- villas/urls.py
+++ villas/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import VillaDetailView, VillaListCreateView
+from .views import VillaDetailView, VillaListCreateView, AvailabilityCreateView
 
 urlpatterns = [path('', VillaListCreateView.as_view(), name='villa-list-create'),
-               path('<int:pk>/', VillaDetailView.as_view(), name='villa-detail')]
-
+               path('<int:pk>/', VillaDetailView.as_view(), name='villa-detail'),
+               path('<int:villa_id>/availability', AvailabilityCreateView.as_view(), name='availability-create')]
--- villas/views.py
+++ villas/views.py
@@ -4,6 +4,13 @@ from .models import Villa
 from .serializers import VillaSerializer
 from .permissions import IsHost
 
+from .serializers import AvailabilitySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Availability
+
+
 class VillaListCreateView(generics.ListCreateAPIView):
 
     serializer_class = VillaSerializer
@@ -38,3 +45,35 @@ class VillaDetailView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
 
+
+
+
+
+class AvailabilityCreateView(APIView):
+
+    permission_classes = [permissions.IsAuthenticated, IsHost]
+
+    def post(self, request, villa_id):
+
+        try:
+            villa = Villa.objects.get(id = villa_id)
+
+        except (Villa.DoesNotExist):
+            return Response({'detail': 'Villa not found.'}, status= status.HTTP_404_NOT_FOUND)
+        
+        if(villa.host != request.user):
+            return Response({'detail': 'You can\'t manage availability for others villas.'}, status=status.HTTP_403_FORBIDDEN)
+
+        dates = request.data.get('dates', [])
+
+        if(len(dates) == 0):
+            return Response({'detail': 'dates list required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        listt = []
+
+        for date in dates:
+            availability, exist = Availability.objects.get_or_create(villa = villa, date = date, defaults={'is_available': True})
+            listt.append(availability)
+
+        serializer = AvailabilitySerializer(listt, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

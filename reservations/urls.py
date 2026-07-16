91fdd66b0eac9a1715480791a6b82554a92bf971
soroush <soroush8506@gmail.com>
2026-07-17 03:03:36 +0330

    added the ability to see the reservations for host

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import ReservationListCreateView
+from .views import ReservationListCreateView, VillaReservationsView
 
-urlpatterns = [path('', ReservationListCreateView.as_view(), name='reservation-list-create')]
+urlpatterns = [path('', ReservationListCreateView.as_view(), name='reservation-list-create'),
+               path('villa/<int:villa_id>/', VillaReservationsView.as_view(), name='villa-reservations')]

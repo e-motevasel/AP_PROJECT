1f8a306aa8dc20736b89a15eaaa4af005db909fe
Ehsan <e.motevasel@gmail.com>
2026-07-17 01:40:43 +0330

    fixed a typo in urls.py of villas

--- villas/urls.py
+++ villas/urls.py
@@ -3,4 +3,4 @@ from .views import VillaDetailView, VillaListCreateView, AvailabilityCreateView
 
 urlpatterns = [path('', VillaListCreateView.as_view(), name='villa-list-create'),
                path('<int:pk>/', VillaDetailView.as_view(), name='villa-detail'),
-               path('<int:villa_id>/availability', AvailabilityCreateView.as_view(), name='availability-create')]
+               path('<int:villa_id>/availability/', AvailabilityCreateView.as_view(), name='availability-create')]
